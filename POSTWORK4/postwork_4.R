569abde68dced2fc9bc81cc53eb36d01f2fc0c3c
OmiLab-dev <62299510+OmiLab-dev@users.noreply.github.com>
2021-02-01 12:58:07 -0600

    Update postwork_4.R

--- POSTWORK4/postwork_4.R
+++ POSTWORK4/postwork_4.R
@@ -1,44 +1,22 @@
-#Solución al Postwork Sesión 4
-
+#POSTWORK 4
 
 library(dplyr)
 library(reshape2)
 library(ggplot2)
 
 data <- read.csv("datos.csv")
-dim(data)
-str(data)
-tail(data)
-View(data)
-summary(data)
 
-#Con ayuda de la función table obtenemos las estimaciones de probabilidades solicitadas
-(pcasa <- round(table(data$FTHG)/dim(data)[1], 3)) # Probabilidades marginales estimadas para los equipos que juegan en casa
-
-(pvisita <- round(table(data$FTAG)/dim(data)[1], 3)) # Probabilidades marginales estimadas para los equipos que juegan como visitante
-
-(pcta <- round(table(data$FTHG, data$FTAG)/dim(data)[1], 3)) # Probabilidades conjuntas estimadas para los partidos
-
-#Con la función apply primero dividimos cada elemento de las columnas de la matriz de probabilidades conjuntas, por las probabilidades marginales asociadas y que corresponden al equipo de casa. Note como hemos definido una función anómima dentro de apply. Luego dividimos cada elemento de las filas de la matriz que resulta, por las probabilidades marginales asociadas y que corresponden al equivo visitante. Finalmente hacemos obtenemos la transpuesta de la matriz que resulta. Esta última matriz, es la matriz de cocientes buscada, es decir, hemos dividio cada probabilidad conjunta, por el producto de probabilidades marginales correspondientes.
-(cocientes <- apply(pcta, 2, function(col) col/pcasa))
-
-(cocientes <- apply(cocientes, 1, function(fila) fila/pvisita))
-
+(probcasa <- round(table(data$FTHG)/dim(data)[1], 3)) 
+(probvisita <- round(table(data$FTAG)/dim(data)[1], 3)) 
+(probconjunta <- round(table(data$FTHG, data$FTAG)/dim(data)[1], 3)) 
+(cocientes <- apply(probconjunta, 2, function(col) col/probcasa))
+(cocientes <- apply(cocientes, 1, function(fila) fila/probvisita))
 (cocientes <- t(cocientes))
 
-#Primero extraemos de manera aleatoria algunas filas de nuestro data frame data, esto lo hacemos con ayuda de la función sample.
 set.seed(2)
 indices <- sample(dim(data)[1], size = 380, replace = TRUE)
 newdata <- data[indices, ]
-
-#Con ayuda de la función table obtenemos las estimaciones de probabilidades
-
-(pcasa <- round(table(newdata$FTHG)/dim(newdata)[1], 3)) # Probabilidades marginales estimadas para los equipos que juegan en casa
-
-(pvisita <- round(table(newdata$FTAG)/dim(newdata)[1], 3)) # Probabilidades marginales estimadas para los equipos que juegan como visitante
-
-(pcta <- round(table(newdata$FTHG, newdata$FTAG)/dim(newdata)[1], 3)) # Probabilidades conjuntas estimadas para los partidos
-
-#Obtenemos nuevamente los cocientes de probabilidades conjuntas entre probabilidades marginales
-
+(pcasa <- round(table(newdata$FTHG)/dim(newdata)[1], 3)) 
+(pvisita <- round(table(newdata$FTAG)/dim(newdata)[1], 3)) 
+(pcta <- round(table(newdata$FTHG, newdata$FTAG)/dim(newdata)[1], 3)) 
 (cocientes <- pcta/outer(pcasa, pvisita, "*"))
